2aa7f9f34f1f142f720e42096506f1281b11d808
Belen Garijo Taillefer <begarijo@c3r10s3.42malaga.com>
2023-08-23 18:33:30 +0200

    fin dia3

--- src/builtins.c
+++ src/builtins.c
@@ -3,41 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marirodr <marirodr@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: begarijo <begarijo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 13:43:04 by begarijo          #+#    #+#             */
-/*   Updated: 2023/08/23 11:31:26 by marirodr         ###   ########.fr       */
+/*   Updated: 2023/08/23 18:29:28 by begarijo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	ft_is_builtin(char **args)
+int	ft_is_builtin(t_data *data)
 {
-	if (ft_strcmp(args[0], "echo") == 0)
+	if (ft_strcmp(data->args[0], "echo") == 0)
 		return (1);
-	if (ft_strcmp(args[0], "cd") == 0)
+	if (ft_strcmp(data->args[0], "cd") == 0)
 		return (2);
-	if (ft_strcmp(args[0], "pwd") == 0)
+	if (ft_strcmp(data->args[0], "pwd") == 0)
 		return (3);
-	if (ft_strcmp(args[0], "export") == 0)
+	if (ft_strcmp(data->args[0], "export") == 0)
 		return (4);
-	if (ft_strcmp(args[0], "unset") == 0)
+	if (ft_strcmp(data->args[0], "unset") == 0)
 		return (5);
-	if (ft_strcmp(args[0], "env") == 0)
+	if (ft_strcmp(data->args[0], "env") == 0)
 		return (6);
-	if (ft_strcmp(args[0], "exit") == 0)
+	if (ft_strcmp(data->args[0], "exit") == 0)
 		return (7);
 	return (0);
 }
 
-void	ft_do_builtins(char **args, t_data *data)
+void	ft_do_builtins(t_data *data)
 {
 	int	b;
 
-	b = ft_is_builtin(args);
+	b = ft_is_builtin(data);
 	if (b == 1)
-		ft_echo(args);
+		ft_echo(data);
 	else if (b == 2)
 		ft_cd(data);
 	else if (b == 3)
@@ -47,7 +47,7 @@ void	ft_do_builtins(char **args, t_data *data)
 	else if (b == 5)
 		printf("unseteame esta\n");
 	else if (b == 6)
-		ft_env(data, args);
+		ft_env(data, data->args);
 	else if (b == 7)
 		printf("HAZ CTRL-D VAGO");
 }
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marirodr <marirodr@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: begarijo <begarijo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 15:53:27 by marirodr          #+#    #+#             */
-/*   Updated: 2023/08/23 12:31:50 by begarijo         ###   ########.fr       */
+/*   Updated: 2023/08/23 18:26:38 by begarijo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_data	*ft_init_data(char **env)
 
 void	ft_start_minishell(t_data *data)
 {
-	while(1)
+	while (1)
 	{
 		data->input = readline("PutaShell> ");
 		if (data->input == NULL)
@@ -40,8 +40,8 @@ void	ft_start_minishell(t_data *data)
 		add_history(data->input);
 		data->args = ft_split(data->input, ' ');
 		data->argc = ft_double_pointer_len(data->args);
-		if (ft_is_builtin(data->args))
-			ft_do_builtins(data->args, data);
+		if (ft_is_builtin(data))
+			ft_do_builtins(data);
 	}
 }
 
